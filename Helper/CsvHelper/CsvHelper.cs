6eff6423b3e633fc349ffe90a7b25b522d68679c
yo <099122@gmail.com>
2024-03-10 16:37:29 +0800

    0310

---
+++ Helper/CsvHelper/CsvHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace AppModbusTcp.Helper.CsvHelper
+{
+    public class TagData
+    {
+        public string Description { get; set; }
+        public int Address { get; set; }
+        public string TagName { get; set; }
+        public string Line { get; set; }
+    }
+    internal class MyCsvHelper
+    {
+        public  List<TagData> ReadCsvFile(string filePath)
+        {
+            List<TagData> tagDataList = new List<TagData>();
+
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+
+
+                while (csv.Read())
+                {
+                    // Map CSV fields to TagData object
+                    TagData tagData = csv.GetRecord<TagData>();
+                    tagDataList.Add(tagData);
+                }
+            }
+
+            return tagDataList;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,74 +18,20 @@ using System.Threading;
 using System.Threading.Tasks;
 using app1;
 using System.Data;
-
+using AppModbusTcp.Helper.CsvHelper;
 /*1.寫入csv檔5天
  *2.
  * 123
  */
 string CIM_CTLPLC_IP = "192.168.2.3";
-ushort BitStartAddress = 00000;
-ushort startAddress_D = 6000;
 ushort startAddress_R = 1000;
-
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config/config.csv");
 NModbusRepository _NModbusRepository1 = new NModbusRepository();
+MyCsvHelper _MyCsvHelper = new MyCsvHelper();
 
-ThreadStart td_start1 = new ThreadStart(_Command);
-new Thread(td_start1).Start();
 ThreadStart td_start2 = new ThreadStart(run2);
 new Thread(td_start2).Start();
 bool _Process = false;
-void _Command()
-{
-    while (2 > 1)
-    {
-        string _StrConsole = "";
-        _StrConsole = Console.ReadLine().Trim().Replace(" ","");
-        Console.BackgroundColor = ConsoleColor.Black;
-        if (_StrConsole.Length >= 3)
-        {
-            if ((_StrConsole.Substring(0, 1) == "D")&&(_StrConsole.IndexOf('=')>0))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(_StrConsole);
-
-                int xx= _StrConsole.Trim().IndexOf('=');
-                string[] _array = _StrConsole.Split('=');
-                string _tagName = _StrConsole.Trim().Substring(xx - 1, 1);
-                _Process = false;
-                startAddress_D = ushort.Parse("600" + ushort.Parse(_tagName));
-                _NModbusRepository1.ModbusTcpMasterWriteHoldingRegisters(CIM_CTLPLC_IP, startAddress_D, UInt16.Parse(_array[1]));
-              //  _NModbusRepository1.ModbusTcpMasterWriteHoldingRegisters(CIM_CTLPLC_IP, ushort.Parse("6000" + _array[0].Substring(1, 1)), ushort.Parse(System.DateTime.Now.Second.ToString()));
-
-            }
-            if ((_StrConsole.Substring(0, 1) == "Y") && (_StrConsole.IndexOf('=') > 0))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(_StrConsole);
-
-                int xx = _StrConsole.Trim().IndexOf('=');
-                string[] _array = _StrConsole.Split('=');
-                string _tagName = _StrConsole.Trim().Substring(xx - 1, 1);
-                _Process = false;
-                startAddress_D = ushort.Parse("000" + ushort.Parse(_tagName));
-                _NModbusRepository1.ModbusTcpMasterWriteHoldingRegisters(CIM_CTLPLC_IP, startAddress_D, UInt16.Parse(_array[1]));
-                if (_array[1] == "1")
-                {
-                    _NModbusRepository1.ModbusTcpMasterWriteCoils(CIM_CTLPLC_IP, startAddress_D, true);
-                }
-                else
-                {
-                    _NModbusRepository1.ModbusTcpMasterWriteCoils(CIM_CTLPLC_IP, startAddress_D, false);
-                }
-                //  _NModbusRepository1.ModbusTcpMasterWriteHoldingRegisters(CIM_CTLPLC_IP, ushort.Parse("6000" + _array[0].Substring(1, 1)), ushort.Parse(System.DateTime.Now.Second.ToString()));
-
-            }
-            _Process = true;
-            Console.ForegroundColor = ConsoleColor.White;
-
-        }
-    }
-}
 
 
 void run2()
@@ -97,70 +43,28 @@ void run2()
         Console.Clear();
         Console.WriteLine(DateTime.Now.ToString());
         Console.WriteLine($"********************************************************************************************************");
-      
+        Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+       var file= _MyCsvHelper.ReadCsvFile(filePath);
+    
         try
         {
 
-            //_NModbusRepository1.ModbusTcpMasterWriteCoils(CIM_CTLPLC_IP, BitStartAddress, true);//Y2
-          bool[] _BitArray_Y = _NModbusRepository1.ModbusTcpMasterReadCoils(CIM_CTLPLC_IP, BitStartAddress, 30);
-            ushort[] _DataArray_D = _NModbusRepository1.ModbusTcpMasterReadHoldingRegisters(CIM_CTLPLC_IP, startAddress_D, 10);
-            ushort[] _DataArray_R = _NModbusRepository1.ModbusTcpMasterReadHoldingRegisters(CIM_CTLPLC_IP, startAddress_R, 10);
+        
+            ushort[] _DataArray_R = _NModbusRepository1.ModbusTcpMasterReadHoldingRegisters(CIM_CTLPLC_IP, 1000, 10);
 
-            //  _NModbusRepository1.ModbusTcpMasterWriteHoldingRegisters(CIM_CTLPLC_IP, DatastartAddress, 777);
+           ushort[] _DataArray_R2 = _NModbusRepository1.ModbusTcpMasterReadHoldingRegisters(CIM_CTLPLC_IP, 2000, 10);
 
-            //  _NModbusRepository1.ModbusTcpMasterWriteHoldingRegisters(CIM_CTLPLC_IP, 9500, 12345);
+            ushort[] _DataArray_R3 = _NModbusRepository1.ModbusTcpMasterReadHoldingRegisters(CIM_CTLPLC_IP, 300, 5);
 
+            bool[] _DataArray_X = _NModbusRepository1.ModbusTcpMasterReadCoils(CIM_CTLPLC_IP,1001, 35);
 
-           
+          //  bool[] _x2 = _NModbusRepository1.ModbusTcpMasterReadInputs(CIM_CTLPLC_IP, 1001, 35);
+            foreach (var i in _DataArray_X)
+            {
 
-
-            string _StrY = "";
-            for (int i = 0; i < _BitArray_Y.Length - 1; i++)
-              {
-                  string _StrShow = "Y" + i + $":0000{(BitStartAddress + i + 1)}={(_BitArray_Y[i] ? 1 : 0)} ";
-
-                  if (i < 10)
-                  {
-                      _StrY = _StrY + _StrShow + ","; ;
-                  }
-                  else
-                  {
-                      //    _StrY = "";
-                  }
-                  //  Console.WriteLine(_StrShow);
-              }
-            //  SaveToCSV(_StrY);
-            //  Console.WriteLine(_StrY);
-
-              Console.WriteLine($"                                                    ");
-
-              // TagList _TagList1=new TagList();
-              //_TagList1.
-
-
-              string _StrD = "";
-              for (int i = 0; i < _DataArray_D.Length - 1; i++)
-              {
-
-                  //   csv.WriteRecords(('\t' + dataarray[i].ToString()));
-                  // csv.NextRecord();
-                  string _StrShow2 = "D" + i + $":4{(startAddress_D + i + 1)}={(_DataArray_D[i].ToString())}  ";
-                  if (i < 10)
-                  {
-                      _StrD = _StrD + _StrShow2 + ",";
-                  }
-                  else
-                  {
-                      //    _StrY = "";
-                  }
-
-              }
-             // SaveToCSV(_StrD);
-              // csv.WriteRecords(_StrD);
-              // csv.NextRecord();
-             // Console.WriteLine(DateTime.Now.ToString() + "," + _StrD);
-              
-              string _StrR = "";
+            Console.WriteLine($"X: {i}");
+            }
+            string _StrR = "";
               for (int i = 0; i < _DataArray_R.Length - 1; i++)
               {
 
@@ -175,9 +79,37 @@ void run2()
                       //    _StrY = "";
                   }
               }
-            SaveToCSV(_StrR);
-            int _dr = startAddress_D;
+           // SaveToCSV(_StrR);
+         for (int i = 0; i < _DataArray_R2.Length - 1; i++)
+            {
 
+
+                string _StrShow2 = "R" + i + $":4000{(startAddress_R + i + 1)}={(_DataArray_R[i].ToString())}  ";
+                if (i < 10)
+                {
+                    _StrR = _StrR + _StrShow2;
+                }
+                else
+                {
+                    //    _StrY = "";
+                }
+            }
+            for (int i = 0; i < _DataArray_R3.Length - 1; i++)
+            {
+
+
+                string _StrShow2 = "R" + i + $":4000{(startAddress_R + i + 1)}={(_DataArray_R[i].ToString())}  ";
+                if (i < 10)
+                {
+                    _StrR = _StrR + _StrShow2;
+                }
+                else
+                {
+                    //    _StrY = "";
+                }
+            }
+            SaveToCSV(_StrR);
+            // SaveToCSV(_StrR);
             SaveToCSV("更新時間:" + DateTime.Now.ToString());
             SaveToCSV($"------------------------------------------------------------");
         }
@@ -208,7 +140,7 @@ void run2()
         catch { }
 
     }
-    void ExportJson()
+   /* void ExportJson()
     {
         using var Writer = new StreamWriter("123.json", false, CodePagesEncodingProvider.Instance.GetEncoding("Big5"));
 
@@ -261,7 +193,7 @@ void run2()
 
 
 
-    }
+    }*/
 }
 
 
